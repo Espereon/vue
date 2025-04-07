77105e7790f98c145f67fe8321194eced7a6409b
Espereon <announcement32@post.com>
2025-04-07 17:49:59 +0300

    l6_sem

---
+++ lesson_6_sem/src/components/MyUser.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <label for="name">
+            Имя: <input type="text" id="name" v-model="firstName">
+        </label>
+        <label for="lastName">
+            Фамилия: <input type="text" id="lastName" v-model="lastName">
+        </label>
+        <div>Почта {{ userData.email }}</div>
+        <button @click="toggleInfo">Shom more</button>
+        <div v-if="showMore">
+            <div>телефон: {{ userData.phone }}</div>
+            <div>адрес: {{ userData.adres }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyUser',
+    props: ['userData'],
+    data() {
+        return {
+            showMore: false,
+            firstName: '',
+            lastName: ''
+        }
+    },
+    methods: {
+        toggleInfo() {
+            this.showMore = !this.showMore;
+        }
+    },
+    created() {
+        this.firstName = this.userData.firstName;
+        this.lastName = this.userData.lastName
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ lesson_6_sem/src/components/UserList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <MyUser v-for="user in users" :key="user.email" :user-data="user" />
+    </div>
+</template>
+
+<script>
+import MyUser from './MyUser.vue';
+export default {
+    name: 'UserList',
+    data() {
+        return {
+            users: [
+                { firstName: 'Иван', lastName: 'Иванов', email: 'ivanov@example.com', phone: '880-555-33-44', adres: 'Бульвар бублика' },
+                { firstName: 'Петр', lastName: 'Петров', email: 'petrov@example.com', phone: '880-555-33-44', adres: 'Дом ступника' },
+                { firstName: 'Анна', lastName: 'Смирнова', email: 'smirnova@example.com', phone: '880-555-33-44', adres: 'Квартира Укупинка' }
+            ]
+        }
+    },
+    components: { MyUser }
+}
+</script>
+
+<style lang="scss" scoped></style>
