dd0a2ea563f02022dbd8635b91cf58819ad20c44
Espereon <announcement32@post.com>
2025-04-14 01:35:37 +0300

    np_1

--- new_project/src/App.vue
+++ new_project/src/App.vue
@@ -1,10 +1,52 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
 import Header from "./components/Header.vue";
+import CardList from "./components/CardList.vue";
+import Drawer from "./components/Drawer.vue";
+
+const items = ref([]);
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      "https://66cc62f5ea4e5b9f.mokky.dev/items"
+    );
+    items.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-20">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <!-- <Drawer /> -->
     <Header />
+    <div class="p-10">
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border border-gray-100 rounded-md">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" />
+            <input
+              class="border border-gray-100 rounded-md py-2 pl-11 pr-4 focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
+    </div>
   </div>
 </template>
 
